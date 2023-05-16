2c2f1cdedd1bdefd42ffbed7387174a865ffe5eb
Ankit Kumar <ankitgandral007@gmail.com>
2023-05-16 12:36:31 +0000

    adding new commit

--- networking.tf
+++ networking.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main-vpc" {
-cidr_block = "10.3.0.0/16"
+cidr_block = var.vpc_cidr_block
 enable_dns_hostnames = "true"
 tags = {
-Name = "student.3-vpc"
+Name = var.vpc_name
 }
 }
 
@@ -11,14 +11,14 @@ vpc_id = aws_vpc.main-vpc.id
 cidr_block = var.subnet-1_cidr_block
 map_public_ip_on_launch = "true"
 tags = {
-Name = "student.3-subnet-1"
+Name = var.subnet-1_name
 }
 }
 
 resource "aws_internet_gateway" "igw" {
 vpc_id = aws_vpc.main-vpc.id
 tags = {
-Name = "student.3-igw"
+Name = var.igw_name
 }
 }
 
@@ -30,7 +30,7 @@ cidr_block = "0.0.0.0/0"
 gateway_id = aws_internet_gateway.igw.id
 }
 tags = {
-Name = "student.3-route-table"
+Name = var.route_table_name
 }
 }
 
--- variable.tf
+++ variable.tf
@@ -1,3 +1,41 @@
 variable "subnet-1_cidr_block" {
 default = "10.3.0.0/24"
 }
+
+variable "region" {
+ default = "us-west-2"
+}
+
+variable "profile" {
+ default = "student.3"
+}
+
+variable "vpc_cidr_block" {
+ default = "10.3.0.0/16"
+}
+
+variable "vpc_name" {
+ default = "student.3-vpc"
+}
+
+variable "subnet-1_name" {
+ default = "student.3-subnet-1"
+}
+
+variable "igw_name" {
+ default = "student.3-igw"
+}
+
+variable "route_table_name" {
+ default = "student.3-route-table"
+}
+
+variable "security_group_name" {
+ default = "student.3-security-group"
+}
+
+variable "key_name" {
+ default = "student.3-vm-key"
+}
+
+
